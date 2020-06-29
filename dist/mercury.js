47efdefc92628551aa3e3a305b85ec1a71a675ca
Nick Wynja <nick@nickwynja.com>
2020-06-29 16:41:01 -0400

    SAVEPOINT

--- dist/mercury.js
+++ dist/mercury.js
@@ -1897,17 +1897,29 @@ var NYTimesExtractor = {
         src = src.replace('{{size}}', width);
         $node.attr('src', src);
       },
-      'figure': function figure($node, $) {
+      figure: function figure($node, $) {
         // const imgCaption = $node.find('figcaption');
         var imgSrc = $node.attr('itemid');
         var imgContainer = $node.find('div[data-testid="lazyimage-container"]');
         var $img = $('<img />').attr('src', imgSrc);
         var figChild = $node.children().children().first();
 
-        if (figChild.text() === "Image") {
+        if (figChild.text() === 'Image') {
           figChild.remove();
         }
+
         imgContainer.replaceWith($img);
+      },
+      h2: function h2($node) {
+        // The "id" attribute values would result in low scores and the element being
+        // removed.
+        $node.attr('id', null);
+        $node.attr('class', null); // A heading will be removed if there is not a paragraph before it, so
+        // add a paragraph here. It will be removed anyway because it is empty.
+
+        $node.before('<p></p>'); // h1 elements will be demoted to h2, so demote h2 elements to h3.
+
+        return 'h3';
       }
     },
     clean: ['.ad', 'header#story-header', '.story-body-1 .lede.video', '.visually-hidden', '#newsletter-promo', '.promo', '.comments-button', '.hidden', '.comments', '.supplemental', '.nocontent', '.story-footer-links', '#styln-faq-coronavirus']

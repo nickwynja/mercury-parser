073457ea0da4ac4e463b71d7fcc490b881eca069
Nick Wynja <nick@nickwynja.com>
2020-07-09 10:18:13 -0400

    SAVEPOINT

--- dist/mercury.js
+++ dist/mercury.js
@@ -1994,7 +1994,9 @@ var NewYorkerExtractor = {
     selectors: ['article[class*="main-content"] div[class^="content-background"]'],
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
-    transforms: [],
+    transforms: {
+      'noscript': 'div'
+    },
     // Is there anything that is in the result that shouldn't be?
     // The clean selectors will remove anything that matches from
     // the result
--- src/extractors/custom/www.newyorker.com/index.js
+++ src/extractors/custom/www.newyorker.com/index.js
@@ -26,7 +26,9 @@ export const NewYorkerExtractor = {
 
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
-    transforms: [],
+    transforms: {
+      noscript: 'div',
+    },
 
     // Is there anything that is in the result that shouldn't be?
     // The clean selectors will remove anything that matches from
